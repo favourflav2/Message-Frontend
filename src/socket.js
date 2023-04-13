6f00a7e776e3a3b30b4672497a73e095266b655e
favourflav <chukwagon1@gmail.com>
2023-04-12 19:08:27 -0700

    edit socket js

--- src/socket.js
+++ src/socket.js
@@ -6,4 +6,4 @@ const {REACT_APP_LOCALHOST_API,REACT_APP_PROD_API} = process.env
 
 const ENDPOINT = "http://localhost:5001"
 
-export const socket = io(ENDPOINT)
+export const socket = io(`${devEnv ? process.env.REACT_APP_LOCALHOST_API : process.env.REACT_APP_PROD_API} `)
